804ee47ba8b794ae98e89c3ee54a70f78a06b7ea
wangbinglun <836414757@qq.com>
2018-05-25 10:30:19 +0800

    1.config.gradle 2.BaseActivity

--- app/src/main/java/com/example/lunger/coordinate_appbar_layout/BaseActivity.java
+++ app/src/main/java/com/example/lunger/coordinate_appbar_layout/BaseActivity.java
@@ -1,30 +1,26 @@
 package com.example.lunger.coordinate_appbar_layout;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public class MainActivity extends BaseActivity {
+/**
+ * Created by Allen on 2018/5/25 0025.
+ */
 
-    @BindView(android.R.id.list)
-    RecyclerView vRecyclerView;
+public class BaseActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar vToolbar;
     private Unbinder mBind;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         mBind = ButterKnife.bind(this);
     }
+
 
     @Override
     protected void onDestroy() {
